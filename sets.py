891acaddd7886d86a0a7433ba84e8d16bf16b686
Balaram <muramallabalaram@gmail.com>
2024-08-29 15:20:47 +0530

    new file

---
+++ sets.py
@@ -0,0 +1,23 @@
+# a={1,2,3,4}
+# b=(3,4,5,6)
+# x=a.intersecti(b)
+# y=a.union(b)
+# z=a.symmetric_difference(b)
+# c=a.difference(b)
+# a.update(b)
+# print(a)
+# print(y)
+# print(x)
+# print(z)
+# print(c)
+# a={1,2,3,4}
+# b={3,4,5,6}
+# a.update(b)
+# a.intersection_update(b)
+# a.difference_update(b)
+# a.symmetric_difference_update(b)
+# print(a)
+a=['a','b','c']
+b=[1,2,3]
+c=dict(zip(a,b))
+print(c)
